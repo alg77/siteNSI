58e37acb6f3663acf932c56d4c483dd324bcc058
amara <a.leguern@stlaurent77.com>
2026-05-18 18:22:53 +0200

    ajout cours archi 1ere
    ajout cours archi 1ere

--- Cours/Tle/nsi-bac-etape4/nsi-bac/scripts/questions/q_archi.js
+++
@@ -1,5 +0,0 @@
-/* Ce fichier charge q_archi_reseaux.js et expose les deux banques */
-(function() {
-  // Les deux banques sont dans q_archi_reseaux.js, déjà chargé
-  // Ce fichier est un alias pour la banque 'archi'
-})();
